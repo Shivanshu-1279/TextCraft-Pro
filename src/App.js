5353b71215a6185a54cfa9cd9481b35f12341a7e
Shivanshu-1279 <92776074+Shivanshu-1279@users.noreply.github.com>
2023-04-17 21:37:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
@@ -6,21 +6,21 @@ import TextForm from './Components/TextForm';
 // import PropTypes from 'prop-types'
 import React, { useState } from 'react';
 import Alert from './Components/Alert'
-// import About from './Components/About';
-
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   BrowserRouter
-// } from "react-router-dom";
+import About from './Components/About';
+                                                              
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // BrowserRouter
+} from "react-router-dom";
 
 function App() {
 
   // Creating Dark mode in app.js file itself
   const [mode, setMode] = useState('light');
-
-  //  setalert is a fucntion ans inside it show =alert is an object.
+          
+  //  setalert is a function and inside it show =alert is an object.
  const [alert, setAlert] = useState(null);
 
  const showAlert = (message,type)=>{
@@ -28,18 +28,28 @@ function App() {
      msg : message,
      type : type
    })
-
+                                                                             
   //  Alert will be vanished within the given time 
   setTimeout(() => {
     setAlert(null);
-  }, 1500);
+  }, 1000);
 }
    
-
- const toggleMode = ()=>{
+// This function will be used to remove the previous background colour.
+const removePrevClasses=()=>{
+  document.body.classList.remove('bg-primary')
+  document.body.classList.remove('bg-success')
+  document.body.classList.remove('bg-danger')
+  document.body.classList.remove('bg-warning')
+}
+                                                    
+ const toggleMode = (cls)=>{
+  console.log(cls);
+  removePrevClasses();
+  document.body.classList.add('bg-' + cls);
      if(mode ==='light'){
        setMode('dark');
-       document.body.style.backgroundColor = '#120324';
+       document.body.style.backgroundColor = 'rgb(32, 7, 62)';
        showAlert("Dark mode has been enabled" , "success");
 
       //  To get some change in title 
@@ -47,41 +57,41 @@ function App() {
       document.title = 'TextUtilis - Dark Mode'
 
       // Now if we want to give any message on title like install ye wo, then within given time. This is specially used in G-Mail to show the messages.
-    setInterval(()=>{
-     document.title = 'Install TextMess now'
-    }, 2000)
+    // setInterval(()=>{
+    //  document.title = 'Install TextMess now'
+    // }, 2000)
      }
-
+                                    
      else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled" , "success");
+      document.title = 'TextUtilis - Light Mode'
+
      }
   }
 
   return (
-    
+          
 <>
-
-  {/* //  <Router> */}
+    <Router>
  <Navbar title={"TextUtlis"}  mode={mode} toggleMode={toggleMode} />
  <Alert alert={alert} />
  <div className="container">
   
- {/* <Switch> */}
+ <Switch>
 
-         {/* <Route exact path="/"> */}
+         <Route exact path="/">
          <TextForm showAlert={showAlert} heading = "Enter the text to analyze" mode={mode} />
-         {/* </Route> */}
-          {/* <Route exact path="/about">
-          <About/>
-          </Route> */}
+         </Route>
+          <Route exact path="/about">
+          <About mode={mode}/>
+          </Route>
             
-      
-        {/* </Switch> */}
+        </Switch>
         
         </div>
-{/* </Router> */}
+</Router>
 </>
 
   );
