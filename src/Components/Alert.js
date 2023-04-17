fdf1c377057bdca4cb599f60e61c6f304f1dc1c6
Shivanshu-1279 <92776074+Shivanshu-1279@users.noreply.github.com>
2023-04-17 21:38:25 +0530

    Update Alert.js

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -7,12 +7,14 @@ function Alert(props) {
   }
   
  return (
-    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+  <div style={{height: '50px'}}>
+   { props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
     <strong>{capitalise(props.alert.type)}</strong>: {props.alert.msg}
 
     {/* {props.alert} */}
-<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div> 
+{/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+</div> }
+</div>
  )
 }
 
