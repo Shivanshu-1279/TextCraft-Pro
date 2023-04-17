9863c5bb13498897281b4ee18b7a3968257bcac8
Shivanshu-1279 <92776074+Shivanshu-1279@users.noreply.github.com>
2023-04-17 21:37:51 +0530

    Update TextForm.js

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,9 +1,9 @@
 // import React from 'react'
 import React, { useState } from 'react';
 
-
 export default function TextForm(props) {
-
+     
+// All the operations are written below as functions 
 const handleUpClick = ()=>{
     // console.log("Uppercase was clicked " + text);
     // Now to convert it in upper case: we'll use
@@ -11,49 +11,61 @@ const handleUpClick = ()=>{
     setText(newText)
     props.showAlert("Converted to Upper Case" , "success");
 }
+                                                                           
 const handleDownClick=()=>{
     let newText2 = text.toLowerCase();
     setText(newText2);
     props.showAlert("Converted to Lower Case" , "success");
 }
-
+                                  
 const handleClearClick=()=>{
     setText("");
     props.showAlert("Cleared" , "success");
 }
 
- const handleOnChange = (event)=>{
+const whiteSpace=()=>{
+  let newText3 = text.trim();
+  setText(newText3);
+  props.showAlert("Removed Extra Space", "success");
+}
+
+const handleOnChange = (event)=>{
     // console.log("on change");
     setText(event.target.value)
- }
+}
 
+  
     // Using state and hooks 
-    // here set has been given a by default value and setText is a function we can use to update it anywhere.
+    // here set has been given by default value and setText is a function we can use to update it anywhere.
     const [text, setText] = useState("");
     // text = "Enter this way"  This is not applicable, we can't use this to change the text 
     // setText("Enter this way")  we can use this way to change the value
 
-    // Whenever we write a Javascript we use "{}" and wehen we use function we use "{}"
+    // Whenever we write a Javascript we use "{}" and when we use function in it we use "{}"
   return (
     <>
     <div>
 <h1 style={{color: props.mode === "dark"?"white": "black" }}>{props.heading}</h1>
 <div className="mb-3">
   {/* <label for="my box" className="form-label">Example textarea</label> */}
-  <textarea className="form-control " value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === "dark"?"grey": "white" , color: props.mode === "dark"?"white": "black" }} id="myBox" rows="8"></textarea>
- <button className="btn btn-primary mx-2 my-3" onClick={handleUpClick}> Convert to UpperCase</button>
- <button className="btn btn-primary mx-4 my-4" onClick={handleDownClick}>Convert on LowerCase</button>
- <button className="btn btn-primary mx-4 my-4" onClick={handleClearClick}>Clear text</button>
-
+  <textarea className="form-control " value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === "dark"?"#321653": "white" , color: props.mode === "dark"?"white": "black" }} id="myBox" rows="8"></textarea>
+ <button disabled={text.length===0} className="btn btn-primary mx-3 my-2" onClick={handleUpClick}> Convert to UpperCase</button>
+ <button disabled={text.length===0} className="btn btn-primary mx-3 my-2" onClick={handleDownClick}>Convert on LowerCase</button>
+ <button disabled={text.length===0} className="btn btn-primary mx-3 my-2" onClick={handleClearClick}>Clear text</button>
+ <button disabled={text.length===0} className="btn btn-primary mx-3 my-2" onClick={whiteSpace}>Remove Extra Space</button>
+                      
+ {/* "disabled" function is used to diable the buttons when nothing is written. */}
+                
 </div>
     </div>
     <div className="container">
         <h2 style={{color: props.mode === "dark"?"white": "black" }}> Text Summary </h2>
-        <p style={{color: props.mode === "dark"?"white": "black" }}>{text.split(" ").length} words and {text.length} characters are present </p>
+        <p style={{color: props.mode === "dark"?"white": "black" }}>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters are present </p>
+        {/* Filter is a high order array function */}
         </div>
         <div className="test">
         <h2 style={{color: props.mode === "dark"?"white": "black" }} >Preview</h2>
-        <p style={{color: props.mode === "dark"?"white": "black" }}>{text.length>0?text : "Enter something in the textbox"}</p>
+        <p style={{color: props.mode === "dark"?"white": "black" }}>{text.length>0?text : "___"}</p>
         </div>
     </>
   )
